7b0de5763f5904d61c1868c1997af6384a4219df
Rishik Murpani <rishik.murpani@leadschool.in>
2025-09-25 03:22:24 +0530

    extra tests

--- src/__tests__/checkout.test.ts
+++ src/__tests__/checkout.test.ts
@@ -9,4 +9,18 @@ describe('Checkout extended scenarios', () => {
     co.scan('atv'); co.scan('atv'); co.scan('atv');
     expect(co.total()).toBe(Number((CATALOG['atv'].price * 2).toFixed(2)));
   });
+
+  test('Should charge for 4 Apple TVs instead of 6', () => {
+    const rules = [new ThreeForTwoRule('atv')];
+    const co = new Checkout(rules);
+    for (let i = 0; i < 6; i++)  co.scan('atv');
+    expect(co.total()).toBe(Number((CATALOG['atv'].price * 4).toFixed(2)));
+  });
+
+  test('Should charge for 2 Apple TVs when buying 2', () => {
+    const rules = [new ThreeForTwoRule('atv')];
+    const co = new Checkout(rules);
+    for (let i = 0; i < 2; i++)  co.scan('atv');
+    expect(co.total()).toBe(Number((CATALOG['atv'].price * 2).toFixed(2)));
+  });
 });
